84a60f9996115a96fc838e5c58163507924739d9
K16DIABLO <miguel92@naver.com>
2022-01-17 16:35:12 +0900

    test minor

--- build/tests/run.sh
+++ build/tests/run.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 export TIME=$(date '+%Y%m%d%H%M')
-K=(10)
-L_SIZE=(3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20)
-#L_SIZE=(20 21 22 23 24 25 26 27 28 29 30)
-#K=(100)
-#L_SIZE=(2 3 4 5 6 7 8 9 10)
+#K=(10)
+#L_SIZE=(140)
+#K=(10)
+#L_SIZE=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20)
+K=(100)
+L_SIZE=(200)
+#L_SIZE=(100 110 120 130 140 150 160 170 180 190 200)
 
 if [ "${1}" == "sift1M" ]; then
   if [ ! -f "sift1M.nsg" ]; then
@@ -16,12 +18,13 @@ if [ "${1}" == "sift1M" ]; then
       exit 1
     fi
   fi
-  echo "Perform kNN searching using NSG index"
   for k in ${K[@]}; do
-    for l in ${multiple[@]}; do
-#      declare -i L=k*l
-#      ./test_nsg_optimized_search sift1M/sift_base.fvecs sift1M/sift_query.fvecs sift1M.nsg $L $k sift1M_nsg_result.ivecs sift1M/sift_groundtruth.ivecs > sift1M_search_K${k}_L${L}_${TIME}.log
-      ./test_nsg_optimized_search sift1M/sift_base.fvecs sift1M/sift_query.fvecs sift1M.nsg 20 10 sift1M_nsg_result.ivecs sift1M/sift_groundtruth.ivecs > sift1M_search_${TIME}.log
+    for l_size in ${L_SIZE[@]}; do
+#      declare -i l=k*l_size
+      declare -i l=l_size
+      echo "Perform kNN searching using NSG index (L${l}K${k})"
+      ./test_nsg_optimized_search sift1M/sift_base.fvecs sift1M/sift_query.fvecs sift1M.nsg $l $k sift1M_nsg_result.ivecs \
+      sift1M/sift_groundtruth.ivecs > sift1M_search_L${l}K${k}_${2}.log
     done
   done
 elif [ "${1}" == "gist1M" ]; then
@@ -34,8 +37,14 @@ elif [ "${1}" == "gist1M" ]; then
       exit 1
     fi
   fi
-  echo "Perform kNN searching using NSG index"
-  ./test_nsg_optimized_search gist1M/gist_base.fvecs gist1M/gist_query.fvecs gist1M.nsg 20 10 gist1M_nsg_result.ivecs gist1M/gist_groundtruth.ivecs > gist1M_search_${TIME}.log
+  for k in ${K[@]}; do
+    for l_size in ${L_SIZE[@]}; do
+      declare -i l=k*l_size
+      echo "Perform kNN searching using NSG index (L${l}K${k})"
+      ./test_nsg_optimized_search gist1M/gist_base.fvecs gist1M/gist_query.fvecs gist1M.nsg $l $k gist1M_nsg_result.ivecs \
+      gist1M/gist_groundtruth.ivecs > gist1M_search_L${l}K${k}_${2}.log
+    done
+  done
 elif [ "${1}" == "all" ]; then
   if [ ! -f "sift1M.nsg" ]; then
     echo "Converting sift_200nn.graph kNN graph to sift.nsg"
@@ -54,8 +63,6 @@ elif [ "${1}" == "all" ]; then
       sift1M/sift_groundtruth.ivecs > sift1M_search_L${l}K${k}_${2}.log
     done
   done
-#  echo "Perform kNN searching using NSG index"
-#  ./test_nsg_optimized_search sift1M/sift_base.fvecs sift1M/sift_query.fvecs sift1M.nsg 20 10 sift1M_nsg_result.ivecs sift1M/sift_groundtruth.ivecs > sift1M_search_${TIME}.log
 
   if [ ! -f "gist1M.nsg" ]; then
     echo "Converting gist_400nn.graph kNN graph to gist.nsg"
@@ -74,8 +81,6 @@ elif [ "${1}" == "all" ]; then
       gist1M/gist_groundtruth.ivecs > gist1M_search_L${l}K${k}_${2}.log
     done
   done
-#  echo "Perform kNN searching using NSG index"
-#  ./test_nsg_optimized_search gist1M/gist_base.fvecs gist1M/gist_query.fvecs gist1M.nsg 20 10 gist1M_nsg_result.ivecs gist1M/gist_groundtruth.ivecs > gist1M_search_${TIME}.log
 else
   echo "Please use either 'sift' or 'gist' as an argument"
 fi
